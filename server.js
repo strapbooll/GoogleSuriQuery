a95a8648da269a969fe3bcfe9377abc6ea568e9a
strapbool <rafael_rx@hotmail.com>
2020-03-24 10:41:00 -0300

    Add Cors

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use('/api', require('./src/routes'));
-  
+app.use("/api", require("./src/routes"));
+
 app.listen(process.env.PORT || 3001);
